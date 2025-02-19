6e272b52061793efbd96811f3a64baa5e4a13583
unknown <Python-Homeworks>
2025-02-19 20:57:58 +0500

    list4.py

---
+++ lesson-3/Homeworks/list3.py
@@ -0,0 +1,8 @@
+
+#  Max Element: From a given list, determine the largest element.
+
+
+my_list = [1, 5, 3, 9, 2, 8]
+
+max_element = max(my_list)
+print(max_element)
---
+++ lesson-3/Homeworks/list4.py
@@ -0,0 +1,7 @@
+
+#  Min Element: From a given list, determine the smallest element.
+
+my_list = [4, 2, 8, 1, 9, 3]
+
+min_element = min(my_list)
+print(min_element)
